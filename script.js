f3e0e09b876e55f07d332f63024c76ad54302fa4
Palo11 <134564484+Paloma-Barragan@users.noreply.github.com>
2023-10-05 15:27:38 +0000

    cambios

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let classifier;
 let imageModelURL = 'https://teachablemachine.withgoogle.com/models/LHtpibQJf/'; // URL del modelo de Teachable Machine
 let fileInput;
 let label = "";
+let img;
 
 function preload() {
   classifier = ml5.imageClassifier(imageModelURL + 'model.json'); // Carga el modelo antes de que se cargue la página
@@ -9,32 +10,20 @@ function preload() {
 
 function setup() {
   fileInput = createFileInput(handleFile); // Crea un input de archivo y llama a la función handleFile cuando se selecciona un archivo
-  fileInput.position(0, height); // Establece la posición del input de archivo
+  createCanvas(640, 480); // Crea un canvas de 640x480 píxeles
 }
 
-function draw() {
-  background(0); // Establece el fondo negro
-  fill(255); // Establece el color de relleno blanco
-  textSize(16); // Establece el tamaño de la fuente
-  textAlign(CENTER); // Establece la alineación del texto al centro
-  text(label, width / 2, height - 4); // Muestra la etiqueta de la clasificación en la parte inferior del canvas
-  let emoji = { // Objeto que asocia las etiquetas de clasificación con emojis
-    "Jugador Español": "😊",
-    "Jugador Polaco": "🤣"
-  }[label];
-  textSize(100); // Establece el tamaño de la fuente
-  text(emoji, width / 2, height / 2); // Muestra el emoji correspondiente a la etiqueta de clasificación en el centro del canvas
-}
-
-function classifyImage(img) {
+function classifyImage() {
   classifier.classify(img, gotResult); // Clasifica la imagen y llama a la función gotResult cuando se obtienen los resultados
 }
 
 function handleFile(file) {
   if (file.type === 'image') { // Verifica que el archivo sea una imagen
-    let img = createImg(file.data, ''); // Crea un elemento de imagen y carga el archivo
+    img = createImg(file.data, ''); // Crea un elemento de imagen y carga el archivo
     img.hide(); // Oculta la imagen original
-    classifyImage(img); // Clasifica la imagen
+    img.size(640, 480); // Establece el tamaño de la imagen
+    image(img, 0, 0); // Muestra la imagen en el canvas
+    classifyImage(); // Clasifica la imagen
   } else {
     console.log('El archivo seleccionado no es una imagen.');
   }
@@ -46,4 +35,17 @@ function gotResult(error, results) {
     return;
   }
   label = results[0].label; // Obtiene la etiqueta de clasificación del primer resultado
+  fill(0); // Establece el color de relleno negro
+  textSize(32); // Establece el tamaño de la fuente
+  textAlign(CENTER); // Establece la alineación del texto al centro
+  text(label, width / 2, height - 50); // Muestra la etiqueta de la clasificación en la parte inferior del canvas
+  let player = { // Objeto que asocia las etiquetas de clasificación con los nombres de los jugadores
+    "españoles":"gavi",
+    "polacos":"lewandoski"
+  }[label];
+  textSize(48); // Establece el tamaño de la fuente
+  text(player, width / 2, height / 2); // Muestra el nombre del jugador correspondiente a la etiqueta de clasificación en el centro del canvas
+  if (img) {
+    image(img, 0, 0, width, height / 2); // Muestra la imagen en la parte superior del canvas
+  }
 }
